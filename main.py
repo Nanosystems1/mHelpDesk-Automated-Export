8bd2cbaafe3027554ac80ecf3dd5f189b0b029fb
Nanosystems1 <100807364+Nanosystems1@users.noreply.github.com>
2022-03-30 19:21:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# filename          : main.py
+# description       : Automate exporting of invoices as PDFs
+# author            : Ian Ault
+# email             : service@nanosystems1.com
+# date              : 03-30-2022
+# version           : v1.0
+# usage             : python main.py
+# notes             :
+# license           : MIT
+# py version        : 3.10.2 (must run on 3.6 or higher)
+#==============================================================================
+import selenium
